857055a8bac9142b62d61e828980671f8231cf82
TzBlitz <Kenthonyg26@gmail.com>
2022-12-13 00:36:11 -0500

    changing assets and adding custom ones

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,9 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState } from 'react';
 import logo from '../assets/img/logo.svg'
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
 
 function NavBar() {
     const [activeLink, setActiveLink] = useState('home');
@@ -29,7 +32,7 @@ function NavBar() {
     <Navbar expand="lg" className={scrolled ? "scrolled":""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="Logo"/>
+            <img src={logo} alt="Logo"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className ="navbar-toggler-icon"></span>
@@ -42,9 +45,9 @@ function NavBar() {
           </Nav>
           <span className ="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={} alt = "" /></a>
-                <a href="#"><img src={} alt = "" /></a>
-                <a href="#"><img src={} alt = "" /></a>
+                <a href="#"><img src={navIcon1} alt = "" /></a>
+                <a href="#"><img src={navIcon2} alt = "" /></a>
+                <a href="#"><img src={navIcon3} alt = "" /></a>
             </div>
             <button className ="vvd" onClick={() => console.log("connect")}><span>Let's Connect</span></button>
           </span>
