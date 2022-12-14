06c7c29ca2706fda5ae1121ae577418e2788b23b
TzBlitz <Kenthonyg26@gmail.com>
2022-12-13 20:54:10 -0500

    Implementation of the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
