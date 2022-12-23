7f1dc01de8fb74d760cc7fec9c3074ec2366ab37
TzBlitz <Kenthonyg26@gmail.com>
2022-12-23 15:16:55 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar.js';
 import { Banner } from './components/Banner.js';
+import { Footer } from "./components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <NavBar/>
       <Banner/>
+      <Footer/>
     </div>
   );
 }
