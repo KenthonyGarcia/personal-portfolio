833b6a4f19a180364b0dab027c998f748f8753de
TzBlitz <Kenthonyg26@gmail.com>
2022-12-23 15:13:11 -0500

    Edited Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col} from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/header-img.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
@@ -44,10 +46,15 @@ export const Banner = () => {
                         <span className="tagline">Welcome to my Portfolio</span>
                         <h1>{"Hi I'm Kenthony "}<span className="wrap">{text}</span></h1>
                         <p>About Me</p>
-                        <butoon onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></butoon>
+                        <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="Header Img"/>
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                            <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                                <img src={headerImg} height = "550px" alt="Header Img"/>
+                            </div>}
+                        </TrackVisibility>
                     </Col>
                 </Row>
             </Container>
